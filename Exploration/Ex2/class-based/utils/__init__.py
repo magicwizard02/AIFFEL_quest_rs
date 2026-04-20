00db0b35bc112b1d9056c0c0ae660a7df3fbbf7a
Elina Choi <dpyun08@gmail.com>
2026-04-21 00:39:37 +0900

    notes on hyperparameter tuning

--- Exploration/Ex2/class-based/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Exposes the train_and_validate function to the package level.
-'''
-from .trainer import train_and_validate
-
-# in-line comment: This allows 'from utils import train_and_validate' in the notebook.
--- Exploration/Ex3/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Exporting specific augmentation functions for easier access in main scripts
-from .basic_aug import basic_aug
-from .mixup import mixup
-from .cutmix import cutmix
